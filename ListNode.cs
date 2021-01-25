3236c8d3496a06d94998688c757d641941157aee
Tigran <tikmarati@gmail.com>
2021-01-25 23:31:13 +0400

    commited

---
+++ ListNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Enumerable
+{
+    class ListNode<T> : IEnumerable<T>
+    {
+        public ListNode(T item)
+        {
+            value = item;
+        }
+
+        public T value;
+        public ListNode<T> next;
+        private ListNode<T> _cuurentNode;
+
+        public void Add(T item)
+        {
+            if (_cuurentNode == null)
+            {
+                next = new ListNode<T>(item);
+                _cuurentNode = next;
+            }
+            else
+            {
+                _cuurentNode.next = new ListNode<T>(item);
+                _cuurentNode = _cuurentNode.next;
+            }
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this;
+            while(node != null)
+            {
+                yield return node.value;
+                node = node.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listNode = new ListNode<int>(10) { 20, 1000, 40 };
+            int[] arr = listNode.ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
